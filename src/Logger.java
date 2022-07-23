3979df6bf2f6ee98de3316ddfbfb2e42592cc98b
valmsmith39a <valmsmith39a@gmail.com>
2022-07-23 00:42:44 -0700

    Logger, Number of Islands: add comments

--- src/Logger.java
+++ src/Logger.java
@@ -1,40 +1,51 @@
 import java.util.HashMap;
+
 /**
  * Logger Rate Limiter
  * 
- * Resource: 
+ * Resource:
  * https://leetcode.com/problems/logger-rate-limiter/
  * 
- * Problem: 
- *  Design a logger system that receives a stream of messages with timestamps. 
- *  Each unique message should only be printed at most once every 10 seconds. 
- *  All messages in chronological order.
- *  Several messages may arrive at same timestamp. 
+ * Problem:
+ * Design a logger system that receives a stream of messages with timestamps.
+ * Each unique message should only be printed at most once every 10 seconds.
+ * All messages in chronological order.
+ * Several messages may arrive at same timestamp.
  * 
- * Implement: 
- *  Logger() initialize the logger object
- *  shouldPrintMessage(int timestamp, string message) {
- *      if message should be printed given the timestamp return 
- *      true, otherwise return false.
- *  }
+ * Implement:
+ * Logger() initialize the logger object
+ * shouldPrintMessage(int timestamp, string message) {
+ * if message should be printed given the timestamp return
+ * true, otherwise return false.
+ * }
  * 
- * Input: 
- *  [[], [1, "foo"], [2, "bar"], [3, "foo"], [8, "bar"], [10, "foo"], [11, "foo"]]
+ * Input:
+ * [[], [1, "foo"], [2, "bar"], [3, "foo"], [8, "bar"], [10, "foo"], [11,
+ * "foo"]]
  * 
- * Summary of Implementation: 
- *  1. Keep track of message and timestamp in a hash table 
- *      key: message
- *      value: timestamp
- *  2. If message does does already exist, return true. 
- *  3. If message exists and the difference between new timestamp
- *     and old timestamp is >= 10, update timestamp value and return true.
+ * Problem Summary:
+ * Design a logger system:
+ * 1. Receives a stream of messages with timestamps.
+ * [[], [1, "foo"], [2, "bar"], [3, "foo"], [11, "foo"]]
  * 
- * Time Complexity: 
- *  O(1) time: lookup and update of hashtable takes constant time.
+ * 2. Each unique message only printed at most once every 10 seconds.
  * 
- * Space Complexity: 
- *  O(M) space: size of all incoming messages. 
- *  
+ * Summary of Implementation:
+ * 1. Track message and timestamp in a hash table
+ * key: message
+ * value: timestamp
+ * 2. If message does not exist, add the message and timestamp to hash table and
+ * return true.
+ * 3. If message does exist and the difference between new timestamp and old
+ * timestamp is >= 10,
+ * update timestamp value and return true, else return false.
+ * 
+ * Time Complexity:
+ * O(1) time: lookup and update of hashtable takes constant time.
+ * 
+ * Space Complexity:
+ * O(M) space: size of all incoming messages.
+ * 
  */
 public class Logger {
     private HashMap<String, Integer> msgHashMap;
@@ -57,18 +68,18 @@ public class Logger {
         return false;
     }
 
-    public static void main (String[] args) {
-       Object[][] array = new Object[][]{{ 1, "foo"}, { 2, "bar"}, { 3, "foo"}, { 8, "bar"}, { 10, "foo"}, { 11, "foo"}};
-       Logger logger = new Logger();
-       boolean shouldPrintMessage = false;
-       for (int i = 0; i < array.length; i++) {
-           shouldPrintMessage = logger.shouldPrintMessage((int) array[i][0], (String) array[i][1]);
-           if (shouldPrintMessage) {
-               System.out.println(shouldPrintMessage);
-           } else {
-               System.out.println(shouldPrintMessage);
-           }
-       }
-   }
+    public static void main(String[] args) {
+        Object[][] array = new Object[][] { { 1, "foo" }, { 2, "bar" }, { 3, "foo" }, { 8, "bar" }, { 10, "foo" },
+                { 11, "foo" } };
+        Logger logger = new Logger();
+        boolean shouldPrintMessage = false;
+        for (int i = 0; i < array.length; i++) {
+            shouldPrintMessage = logger.shouldPrintMessage((int) array[i][0], (String) array[i][1]);
+            if (shouldPrintMessage) {
+                System.out.println(shouldPrintMessage);
+            } else {
+                System.out.println(shouldPrintMessage);
+            }
+        }
+    }
 }
-
--- src/NumberOfIslands.java
+++ src/NumberOfIslands.java
@@ -1,5 +1,43 @@
-class NumberOfIslands {
-    
+public class NumberOfIslands {
+    /**
+     * Number of Islands
+     * 
+     * Problem:
+     * Given an m x n 2D binary grid with a map of '1's (land) and '0's (water),
+     * return the number of islands.
+     * 
+     * An island is surrounded by water and is formed by connecting adjacent lands
+     * horizontally or vertically.
+     * 
+     * Assume all 4 edges of grid are surrounded by water.
+     * 
+     * Example:
+     * 
+     * Input: grid = [
+     * ["1","1","0","0","0"],
+     * ["1","1","0","0","0"],
+     * ["0","0","1","0","0"],
+     * ["0","0","0","1","1"]
+     * ]
+     * 
+     * Output: 3
+     * 
+     * Problem Summary:
+     * 1. Given a 2 dimensional matrix with '1's and '0's
+     * 2. Cluster of '1's (adjacent to each other) are islands.
+     * 3. Find the number of islands (cluster of '1's).
+     * 
+     * Implementation:
+     * 1. Traverse through each element of the matrix using a double for loop
+     * 2. If encounter a '1', increment numberOfIslands counter, call depth first
+     * search on that element, flip '1' to '0',
+     * and call DFS on each adjacent element as well (up, down, left, right).
+     * This process will flip all the '1's to '0's for a cluster of '1's.
+     * 3. When return to initial double for loop, the entire cluster will be flipped
+     * from '1' to '0',
+     * and that counts as 1 island.
+     **/
+
     void dfs(char[][] grid, int r, int c) {
         int nr = grid.length;
         int nc = grid[0].length;
@@ -12,11 +50,11 @@ class NumberOfIslands {
         dfs(grid, r - 1, c);
         dfs(grid, r + 1, c);
         dfs(grid, r, c - 1);
-        dfs(grid, r, c + 1); 
-    }      
+        dfs(grid, r, c + 1);
+    }
 
     int getNumberOfIslands(char[][] grid) {
-        // no grid or empty grid 
+        // no grid or empty grid
         if (grid == null || grid.length == 0) {
             return 0;
         }
@@ -36,40 +74,39 @@ class NumberOfIslands {
     }
 
     public static void main(String args[]) {
-        char[][] grid1 = new char[][]{{}};
+        char[][] grid1 = new char[][] { {} };
         char[] row1 = new char[] { '1', '1', '1', '1', '0' };
         char[] row2 = new char[] { '1', '1', '0', '1', '0' };
         char[] row3 = new char[] { '1', '1', '0', '0', '0' };
         char[] row4 = new char[] { '0', '0', '0', '0', '0' };
-        grid1 = new char[][] { row1, row2, row3, row4 }; 
+        grid1 = new char[][] { row1, row2, row3, row4 };
 
         System.out.println("Number of Islands");
         System.out.println("Grid 1: ");
 
         for (int i = 0; i < 4; i++) {
-            System.out.println(grid1[i]); 
+            System.out.println(grid1[i]);
         }
 
-        NumberOfIslands numberOfIslands = new NumberOfIslands(); 
-        int totalIslands = numberOfIslands.getNumberOfIslands(grid1); 
-        System.out.println("Total number of islands is: " + totalIslands); 
-        
-        char[][] grid2 = new char[][]{{}};
+        NumberOfIslands numberOfIslands = new NumberOfIslands();
+        int totalIslands = numberOfIslands.getNumberOfIslands(grid1);
+        System.out.println("Total number of islands is: " + totalIslands);
+
+        char[][] grid2 = new char[][] { {} };
         char[] grid2row1 = new char[] { '1', '1', '0', '0', '0' };
         char[] grid2row2 = new char[] { '1', '1', '0', '0', '0' };
         char[] grid2row3 = new char[] { '0', '0', '1', '0', '0' };
         char[] grid2row4 = new char[] { '0', '0', '0', '1', '1' };
-        grid2 = new char[][] { grid2row1, grid2row2, grid2row3, grid2row4 }; 
+        grid2 = new char[][] { grid2row1, grid2row2, grid2row3, grid2row4 };
 
         System.out.println("Number of Islands");
         System.out.println("Grid 2: ");
 
         for (int i = 0; i < 4; i++) {
-            System.out.println(grid2[i]); 
-        }          
-        
-        int totalIslandsGrid2 = numberOfIslands.getNumberOfIslands(grid2); 
-        System.out.println("Total number of islands is: " + totalIslandsGrid2); 
-    }   
-}
+            System.out.println(grid2[i]);
+        }
 
+        int totalIslandsGrid2 = numberOfIslands.getNumberOfIslands(grid2);
+        System.out.println("Total number of islands is: " + totalIslandsGrid2);
+    }
+}
