560d4e7b74fb818ecccf93c9cca0d01c1e909aae
Xian Wee <valmsmith39a@gmail.com>
2022-09-02 23:34:14 -0700

    AStar: initial comments

---
+++ src/AStar.java
@@ -0,0 +1,23 @@
+class AStar {
+	/**
+	 * A* Algorithm
+	 *
+	 * Description:
+	 * 	
+	 * 	f(n) = g(n) + h(n)
+	 *
+	 * 	n: Next node 
+	 * 	g(n): Distance from start node to next node 
+	 *	h(n): Heuristic based distance from next node the destination node 
+	 *
+	 *	Goal is to minimize f(n)
+	 * 
+	 * Context: 
+	 * 	1. Is a search algorithm. 
+	 * 	2. Is a variation of Dijkstra's Algorithm.
+	 * 	3. Is a Greedy algorithm.
+	 *
+	 *
+	 * 	
+	 */
+}
--- src/RandomPickWithWeight.java
+++ src/RandomPickWithWeight.java
@@ -14,7 +14,10 @@ public class RandomPickWithWeight {
 	}
 
 	public int pickIndex() {
-		double target = this.totalSum * Math.random();
+		/* Usually to generate a random number within a range 
+		   randomNumber = Math.random() * (max - min) + min
+		   But in this case min = 0, so Math.random() * (max - 0) + 0 => Math.random() * max */
+		double target = Math.random() * this.totalSum;
 
 		int start = 0, end = this.prefixSums.length;
 		while (start < end) {
