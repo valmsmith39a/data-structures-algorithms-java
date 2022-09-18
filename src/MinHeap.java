ff4051e898a7f7c4e017af727256549c1e0a278f
valmsmith39a <valmsmith39a@gmail.com>
2022-09-18 00:46:33 -0700

    Min Heap - initial implementation

--- src/MinHeap.java
+++ src/MinHeap.java
@@ -1,47 +1,97 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 class MinHeap {
+
     List<Integer> heap = new ArrayList<Integer>();
 
-    public MinHeap(List<Integer> array) {
-        heap = buildHeap(array);            
+    public MinHeap(List<Integer> list) {
+        heap = buildHeap(list);            
     }
 
     /*
      * Build min heap by applying sift down on every single parent node, 
-     * starting from the first parent node from the bottom of the binary heap.  
+     * starting from the last parent node of the binary heap.  
      */
-    public List<Integer> buildHeap(List<Integer> array) {
-        int currentIndex = array.size() - 1;
-        int firstParentIndex = (currentIndex - 1) / 2;
-        for (int i = firstParentIndex; i >= 0; i++) {
-            siftDown(i, array);
+    public List<Integer> buildHeap(List<Integer> list) {
+        int size = list.size();
+        int lastParentIndex = getLastParentIndex(size);
+        for (int i = lastParentIndex; i >= 0; i++) {
+            siftDown(i, size - 1,  list);
         }
-        return array;
+        return heap;
     }
     
-    public void siftDown(int currentIndex, List<Integer> heap) {
-        int child1Index = 2 * currentIndex + 1;
-        while (child1Idx < heap.size()) {
-            int smallerChildIndex = child1index;
-            if ()
+    private int getLastParentIndex(int size) {
+       return (size - 2) / 2; 
+    }
+
+    private void siftDown(int currentIndex, int lastIndex, List<Integer> heap) {
+        int leftChildIndex = getLeftChildIndex(currentIndex, heap);
+        while (leftChildIndex <= lastIndex) {
+            int smallestChildIndex = leftChildIndex;
+            int rightChildIndex = getRightChildIndex(currentIndex, heap);
+            if (rightChildIndex <= lastIndex 
+                && heap.get(rightChildIndex)) {
+                smallestChildIndex = rightChildIndex;
+            }
+            // If current value < value of smallest child, min heap property 
+            // satisfied at that node in binary heap.  
+            if (heap.get(currentIndex) < heap.get(smallestChildIndex)) {
+                break;
+            } else {
+                swap(currentIndex, smallestChildIndex);
+            }
+            currentIndex = smallestChildIndex;
         }
     }
 
-    private int getLeftChildIndex(int parentIndex) { return 2 * parentIndex + 1; }
-    private int getRightChildIndex(int parentIndex) { return 2 * parentIndex + 2; }
-    private int getParentIndex(int childIndex) { return (childIndex - 1) / 2; } 
+    private void siftUp(int currentIndex, List<Integer> heap) {
+        int parentIndex = getParentIndex(currentIndex, List<Integer> heap);
+        while (parentIndex >= 0 && heap.get(parentIndex) > heap.get(currentIndex)) {
+            swap(currentIndex, parentIndex, heap);
+            currentIndex = parentIndex;
+            parentIndex = getParentIndex(currentIndex);
+        }
+    }
 
-    private boolean hasLeftChild(int index, int size) { return getLeftChildIndex(index) < size; }
-    private boolean hasRightChild(int index, int size) { return getRightChildIndex(index) < size; }
+    private void insert(int value) {
+        heap.add(value);
+        siftUp(heap.size() - 1, heap);
+    }
 
-
-
+    // Remove the root node 
+    private int remove() {
+        swap(0, heap.size() - 1, heap);
+        int removedValue = heap.remove(heap.size() - 1);
+        siftDown(0, heap.size() - 1, heap);
+        return removedValue
+    }
     
+    private int getLeftChildIndex(int currentIndex, List<Integer> heap) {
+        return 2 * currentIndex + 1;
+    }
+    
+    private int getRightChildIndex(int currentIndex, List<Integer> heap) {
+        return 2 * currentIndex + 2;
+    }
 
+    private int peek() {
+        return heap.get(0);
+    }
+
+    private int getParentIndex(int childIndex, List<Integer> heap) {
+        return (childIndex - 1) / 2; 
+    }
+   
+    private void printHeap(List<Integer> heap) {
+        for (int i = 0; i < heap.size(); i++) {
+            System.out.print(heap[i] + " "); 
+        }
+    }
     public static void main(String args[]) {
-        System.out.println("hi"); 
-        int[] array = new int[] { 48, 12, 24, 7, 8, -5, 24, 391, 24, 56, 2, 6, 8, 41 };
-        j
+        List<Integer> list = new ArrayList<>(Arrays.asList(48, 12, 24, 7, 8, -5, 24, 391, 24, 56, 2, 6, 8, 41));
+        MinHeap minHeap = new MinHeap(list);
+
     }
 }
