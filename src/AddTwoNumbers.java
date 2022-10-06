8a6b2e837f58e8207b449aaf2be93319c7b61805
valmsmith39a <valmsmith39a@gmail.com>
2022-10-05 22:04:46 -0700

    Add Two Numbers: refactor and comments

--- src/AddTwoNumbers.java
+++ src/AddTwoNumbers.java
@@ -1,55 +1,35 @@
 
-class ListNode {
-    int data;
-    ListNode next;
-
-    ListNode(int data) {
-        this.data = data;
-        next = null;
-    }
-}
-
-class LinkedList {
-    ListNode head;
-
-    LinkedList() {
-        head = null;
-    }
-
-    LinkedList(ListNode node) {
-        head = node;
-    }
-
-    void append(int data) {
-        if (head == null) {
-            head = new ListNode(data);
-            return;
-        }
-        
-        ListNode current = head;
-        while (current.next != null) {
-            current = current.next;
-        }
-        current.next = new ListNode(data);
-    }
-}
-
-class AddTwoNumbers {
+public class AddTwoNumbers {
     /**
+     * Problem: Add Two Numbers
+     * 
+     * Description:
+     * Store 2 numbers in reverse order in 2 linked lists and add them.
+     * Store solution in a linked list in reverse order
+     *
+     * Key insight:
+     * 1. carry (when add 2 numbers) is computed by dividing by 10, because you're
+     * asking
+     * "how many tens, hundreds... am I carrying over to the next place?"
+     * 2. The number in the current place (ones, tens, hundreds place etc) is the
+     * remainder
+     * from dividing by 10, so take the some and modulo 10 to get the remainder.
+     * 
      * Time complexity: O(max(m, n))
-     *  m: length of l1 
-     *  n: length of l2
-     *  Executes as most O(max(m, n)) times. 
+     * m: length of l1
+     * n: length of l2
+     * Executes as most O(max(m, n)) times.
      *
      * Space complexity: O(max(m, n))
-     *  Length of solution list is at most max(m, n) + 1 (including dummy head)     *  -> O(max(m, n))
+     * Length of solution list is at most max(m, n) + 1 (including dummy head) * ->
+     * O(max(m, n))
      *
-     */ 
-    ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         int carry = 0;
         ListNode dummyHead = new ListNode(0);
         ListNode currentNode = dummyHead;
-       
+
         while (l1 != null || l2 != null || carry != 0) {
             int x = l1 == null ? 0 : l1.data;
             int y = l2 == null ? 0 : l2.data;
@@ -67,26 +47,61 @@ class AddTwoNumbers {
         }
         return dummyHead.next;
     }
-    
-    ListNode createLinkedList(int[] values) {
+
+    public ListNode createLinkedList(int[] values) {
         LinkedList list = new LinkedList();
         for (int value : values) {
             list.append(value);
-        } 
+        }
         return list.head;
     }
 
-    void printLinkedList(ListNode head) {
+    public void printLinkedList(ListNode head) {
         ListNode current = head;
-        while(current != null) {
+        while (current != null) {
             System.out.print(current.data);
             current = current.next;
         }
         System.out.println();
+
     }
-    
+
+    public class LinkedList {
+        ListNode head;
+
+        public LinkedList() {
+            // Empty constructor
+        }
+
+        public LinkedList(ListNode node) {
+            head = node;
+        }
+
+        public void append(int data) {
+            if (head == null) {
+                head = new ListNode(data);
+                return;
+            }
+
+            ListNode current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = new ListNode(data);
+        }
+    }
+
+    private class ListNode {
+        int data;
+        ListNode next;
+
+        private ListNode(int data) {
+            this.data = data;
+        }
+    }
+
     public static void main(String[] args) {
-        AddTwoNumbers operations = new AddTwoNumbers(); 
+        AddTwoNumbers operations = new AddTwoNumbers();
         int[] list1 = new int[] { 2, 4, 3 };
         int[] list2 = new int[] { 5, 6, 4 };
         ListNode list1Head = operations.createLinkedList(list1);
@@ -95,8 +110,8 @@ class AddTwoNumbers {
         // 243
         operations.printLinkedList(list1Head);
         // 564
-        operations.printLinkedList(list2Head);   
-        // 708 
+        operations.printLinkedList(list2Head);
+        // 708
         operations.printLinkedList(solutionNode);
     }
 }
