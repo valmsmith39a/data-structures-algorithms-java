fe9d283d9333c7f754b67625bfd9ab0fdbd51463
valmsmith39a <valmsmith39a@gmail.com>
2022-11-10 23:02:43 -0800

    QuickSort: move to own file

--- src/DataStructuresAlgorithms.java
+++ src/DataStructuresAlgorithms.java
@@ -149,111 +149,6 @@ public class DataStructuresAlgorithms {
 	}
 
 	/**
-	 * Quick Sort
-	 * 
-	 * Key ideas:
-	 * 1. Recursively sort numbers with respect to a pivot.
-	 * 2. Move numbers <= pivot to the left of the pivot.
-	 * 3. Move numbers > pivot to the right of the pivot.
-	 * 
-	 * Steps:
-	 * 1. Choose a pivot, create left and right pointers.
-	 * 2. Move numbers <= pivot to the left of the pivot.
-	 * 3. Move numbers > pivot to the right of the pivot.
-	 * 4. Swap number at pivot with the number at the right pointer
-	 * so that pivot will be in it's sorted position.
-	 * 5. Continue on sub-arrays to the left/right of the newly positioned pivot.
-	 * 
-	 * Time Complexity:
-	 * Worst case: O(n^2) time: if pivot splits array in lop-sided manner (most of
-	 * numbers are to the left or right of the pivot).
-	 * Best case: if pivot evenly splits the array (approximately equal numbers on
-	 * each side),
-	 * then we make log n calls of quick sort. Performing O (log n) operations n
-	 * times => O(n log n)
-	 * Average case: O (n log n)
-	 *
-	 * Space Complexity:
-	 * O(log n): run quick sort recursively on smaller sub-array first.
-	 * 
-	 * Example iteration:
-	 * 8, 5, 2, 9, 5, 6, 3
-	 * swap
-	 * 8, 5, 2, 3, 5, 6, 9
-	 * swap
-	 * 6, 5, 2, 3, 5, 8, 9
-	 * 
-	 * pivot value: 8
-	 * 
-	 * leftIdx value:
-	 * 5, 2, 9
-	 * swap
-	 * 5, 2, 3, 5, 6, 9
-	 * 
-	 * rightIdx value:
-	 * 3
-	 * swap
-	 * 9, 6
-	 * 
-	 * key insight: rightIdx always ends up at a number smaller than the pivot
-	 * 
-	 * @param numbers  array of numbers
-	 * @param startIdx starting index
-	 * @param endIdx   ending index
-	 * @return void
-	 */
-	public void quickSort(int[] numbers, int startIdx, int endIdx) {
-		/**
-		 * If startIdx > endIdx, return bc incorrect values.
-		 * If startIdx == endIdx, return bc only 1 number, nothing to sort.
-		 */
-		if (startIdx >= endIdx) {
-			return;
-		}
-		int pivotIdx = startIdx;
-		int leftIdx = startIdx + 1;
-		int rightIdx = endIdx;
-		while (leftIdx < rightIdx) {
-			if (numbers[leftIdx] > numbers[pivotIdx] && numbers[rightIdx] < numbers[pivotIdx]) {
-				swap(numbers, leftIdx, rightIdx);
-			}
-			if (numbers[leftIdx] <= numbers[pivotIdx]) {
-				leftIdx++;
-			}
-			if (numbers[rightIdx] >= numbers[pivotIdx]) {
-				rightIdx--;
-			}
-		}
-		// rightIdx and leftIdx have crossed
-		// number at rightIdx is the last number that is smaller than the pivot number
-		// so we can swap the number at rightIdx and the number at the pivotIdx
-		swap(numbers, pivotIdx, rightIdx);
-
-		// At this point, pivot number is in the correct position,
-		// relative to the other numbers in the array (smaller numbers are to the left
-		// of the pivot, larger numbers are to the right of the pivot).
-		// rightIdx is the index of the newly positioned pivot number.
-
-		// Continue sorting numbers with respect to a pivot on subarrays
-		// to the left/right of the newly positioned pivot number.
-		boolean leftSubarrayIsSmaller = rightIdx - 1 - startIdx < endIdx - (rightIdx + 1);
-
-		if (leftSubarrayIsSmaller) {
-			quickSort(numbers, startIdx, rightIdx - 1);
-			quickSort(numbers, rightIdx + 1, endIdx);
-		} else {
-			quickSort(numbers, rightIdx + 1, endIdx);
-			quickSort(numbers, startIdx, rightIdx - 1);
-		}
-	}
-
-	public void swap(int[] numbers, int i, int j) {
-		int swapTemp = numbers[i];
-		numbers[i] = numbers[j];
-		numbers[j] = swapTemp;
-	}
-
-	/**
 	 * Merge Sort
 	 * 
 	 * Divide and conquer algorithm
@@ -368,16 +263,6 @@ public class DataStructuresAlgorithms {
 		int target = 19;
 		int resultBinarySearch = problemSet.binarySearch(numbers, target, 0, numbers.length);
 		System.out.println("Binary Search. " + "Target is: " + target + " Index of target is: " + resultBinarySearch);
-
-		/**
-		 * Quick Sort
-		 */
-		numbers = new int[] { 8, 5, 2, 9, 5, 6, 3 };
-		problemSet.quickSort(numbers, 0, numbers.length - 1);
-		// Output: 2, 3, 5, 5, 6, 8, 9
-		System.out.print("Quick Sort. Sorted array is: ");
-		problemSet.printNumbersArray(numbers);
-		System.out.println();
 
 		/**
 		 * Merge Sort
