4428ee660e996fb54c8b1b15db881483271ef7c2
valmsmith39a <>
2023-04-15 23:00:18 +0000

    Contains Duplicate

---
+++ src/ContainsDuplicate.java
@@ -0,0 +1,24 @@
+import java.util.Set;
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+
+	public boolean containsDuplicate(int[] nums) {
+		Set<Integer> numSet = new HashSet<>();
+		for (int num : nums) {
+			if (numSet.contains(num)) {
+				return true;
+			}
+			numSet.add(num);
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		int[] nums = [1, 2, 3, 1];
+		ContainsDuplicate cd = new ContainsDuplicate();
+		boolean res = cd.ContainsDuplicate(nums);
+		// Expected: true
+		System.out.println(res);
+	}
+}
