c5d5af78d40d86cab4fbe08773d71ebd70b84a13
Xian Wee <valmsmith39a@gmail.com>
2024-09-23 20:58:20 -0700

    Two Sum and change directories for other files

---
+++ src/Review_2024/TwoSum.java
@@ -0,0 +1,26 @@
+package Review_2024;
+import java.util.Map;
+import java.util.HashMap;
+
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[] { i, numMap.get(complement) };
+            }
+            numMap.put(nums[i], i);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        TwoSum ts = new TwoSum();
+        int[] arr = new int[] { 2, 7, 11, 15 };
+        int target = 9;
+        int[] ans = ts.twoSum(arr, target);
+        System.out.println(ans[0] + " " + ans[1]);
+    }
+}
--- src/TwoSumII.java
+++ src/TwoSumII.java
@@ -1,4 +1,17 @@
-
+/**
+ * Problem: Two Sum II 
+ * 
+ * Key Insights: 
+ * 1. Array is sorted, so can use 2 pointer method
+ *  a. left / right pointers at each end. 
+ *  b. if currentSum < target, can't move right pointer to the left bc then
+ *     currentSum would be even smaller, so must move left pointer to the right by 1.
+ *  c. if currentSum > target, can't move left pointer to the right bc then 
+ *     currentSum would be even greater, so must move right pointer to the left by 1.  
+ * 
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
 public class TwoSumII {
 
     public int[] twoSumII(int[] numbers, int target) {
