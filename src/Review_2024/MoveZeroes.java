cc2b59ce719909be4ee7d36a12058b32b7e15cf9
valmsmith39a <valmsmith39a@gmail.com>
2024-11-26 21:15:16 -0800

    Move Zeroes

---
+++ src/Review_2024/MoveZeroes.java
@@ -0,0 +1,24 @@
+package Review_2024;
+
+/**
+ * Move Zeroes #283
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+public class MoveZeroes {
+
+	public void moveZeroes(int[] nums) {
+		int slow = 0;
+
+		for (int fast = 0; fast < nums.length; fast++) {
+			if (nums[fast] != 0) {
+				int temp = nums[slow];
+				nums[slow] = nums[fast];
+				nums[fast] = temp;
+				slow++;
+			}
+		}
+
+	}
+
+}
