73ba93058832ee80dabb33d274d041a347a74b92
valmsmith39a <valmsmith39a@gmail.com>
2025-02-25 22:06:40 -0800

    Merge 2 lists

--- src/Review_2025/LowestCommonAncestor.java
+++ src/Review_2025/LowestCommonAncestor.java
@@ -4,6 +4,18 @@
  */
 public class LowestCommonAncestor {
 
+	public static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		public TreeNode(int val) {
+			this.val = val;
+			this.left = null;
+			this.right = null;
+		}
+	}
+
 	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
 		if (root == null) {
 			return null;
@@ -23,5 +35,16 @@ public class LowestCommonAncestor {
 			return root;
 		}
 		return left != null ? left : right;
+	}
+
+	public static void main(String[] args) {
+		TreeNode n1 = new TreeNode(1);
+		TreeNode n2 = new TreeNode(2);
+		TreeNode n3 = new TreeNode(3);
+		n1.left = n2;
+		n1.right = n3;
+		LowestCommonAncestor lca = new LowestCommonAncestor();
+		TreeNode lowestCommonAncestorNode = lca.lowestCommonAncestor(n1, n2, n3);
+		System.out.println("Lowest Common Ancestor node is: " + lowestCommonAncestorNode.val);
 	}
 }
---
+++ src/Review_2025/MergeTwoSortedLists.java
@@ -0,0 +1,34 @@
+package Review_2025;
+
+public class MergeTwoSortedLists {
+
+	public static class ListNode {
+		int val;
+		ListNode next;
+
+		public ListNode() {
+			/* no-op */
+		}
+
+		public ListNode(int val) {
+			this.val = val;
+			this.next = null;
+		}
+	}
+
+	public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+		if (list1 == null) {
+			return list2;
+		}
+		if (list2 == null) {
+			return list1;
+		}
+		if (list1.val < list2.val) {
+			list1.next = mergeTwoLists(list1.next, list2);
+			return list1;
+		} else {
+			list2.next = mergeTwoLists(list1, list2.next);
+			return list2;
+		}
+	}
+}
