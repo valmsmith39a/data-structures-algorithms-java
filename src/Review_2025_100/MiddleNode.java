b82897966da80ee7a8bd8d31e4d68fc3bf26a002
valmsmith39a <valmsmith39a@gmail.com>
2025-09-08 21:59:00 -0700

    Middle Node

---
+++ src/Review_2025_100/MiddleNode.java
@@ -0,0 +1,34 @@
+/**
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+public class MiddleNode {
+
+	public ListNode middleNode(ListNode head) {
+		ListNode slow = head;
+		ListNode fast = head;
+
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		return slow;
+	}
+
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode() {
+		}
+
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+}
