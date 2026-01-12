6b9048ed2d7fa35ed2f51743eb4d1066dc7a390c
valmsmith39a <valmsmith39a@gmail.com>
2026-01-11 17:00:11 -0800

    Trapping Rainwater 2

---
+++ src/TrappingRainwater2.java
@@ -0,0 +1,30 @@
+public class TrappingRainwater2 {
+
+	public int trap(int[] height) {
+		if (height == null || height.length < 3)
+			return 0;
+
+		int left = 0, right = height.length - 1;
+		int leftMax = 0, rightMax = 0;
+		int water = 0;
+
+		while (left < right) {
+			if (height[left] < height[right]) {
+				if (height[left] >= leftMax) {
+					leftMax = height[left];
+				} else {
+					water += leftMax - height[left];
+				}
+				left++;
+			} else {
+				if (height[right] >= rightMax) {
+					rightMax = height[right];
+				} else {
+					water += rightMax - height[right];
+				}
+				right--;
+			}
+		}
+		return water;
+	}
+}
